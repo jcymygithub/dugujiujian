b0913cceb4675c10e1a50001a7283c19ef2ed845
jingege <jcypower@163.com>
2019-09-10 18:24:28 +0800

    提交

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -3,5 +3,8 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 	public static void main(String[] args){
 		System.out.println("hah");
+		System.out.println("hah");
+		System.out.println("hah");
+		System.out.println("hah");
 	}
 }
