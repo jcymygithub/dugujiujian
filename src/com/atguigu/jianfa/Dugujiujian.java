bb8ba80d66204b6f1128f8a55a80ff69133f3f24
jcy <jcypower@163.com>
2019-09-10 18:38:27 +0800

    提交

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -2,6 +2,9 @@ package com.atguigu.jianfa;
 
 public class Dugujiujian {
 	public static void main(String[] args){
-		System.out.println("hah");
+		System.out.println("hah22222");
+		System.out.println("hah22222");
+		System.out.println("hah22222");
+		System.out.println("hah22222");
 	}
 }
